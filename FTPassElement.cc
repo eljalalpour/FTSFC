0a778028d33e5006e7e4e2da6be2f017287b6dd5
ejalalpo <el.jalalpour@gmail.com>
2017-05-28 17:48:16 -0400

    Adding FTFastTCPGen!

--- FTPassElement.cc
+++ FTPassElement.cc
@@ -1,5 +1,6 @@
 #include <click/config.h>
 #include <click/args.hh>
+#include <clicknet/ether.h>
 #include "FTPassElement.hh"
 CLICK_DECLS
 
@@ -8,31 +9,48 @@ FTPassElement::FTPassElement() { }
 FTPassElement::~FTPassElement() { }
 
 void FTPassElement::push(int port, Packet *p) {
-    LOG("------------------------------");
-    LOG("Begin FTPassElement");
+    click_chatter("------------------------------");
+    click_chatter("Begin FTPassElement - push");
 
+    const click_ether_vlan *vlan = reinterpret_cast<const click_ether_vlan *>(p->data());
+    VLANId vlan_id = (ntohs(vlan->ether_vlan_tci) & 0xFFF);
+    click_chatter("VLAN_ID: %d", vlan_id);
+
+    click_chatter("End FTPassElement - push");
+    click_chatter("------------------------------");
     //TODO: change port back to zero
     output(0).push(p);
-
-    LOG("End FTPassElement");
-    LOG("------------------------------");
 }
 
 Packet* FTPassElement::simple_action(Packet* p) {
+    click_chatter("------------------------------");
+    click_chatter("Begin FTPassElement - Simple action");
+
+    const click_ether_vlan *vlan = reinterpret_cast<const click_ether_vlan *>(p->data());
+    VLANId vlan_id = (ntohs(vlan->ether_vlan_tci) & 0xFFF);
+    click_chatter("VLAN_ID: %d", vlan_id);
+
+    click_chatter("End FTPassElement - Simple action");
+    click_chatter("------------------------------");
     return p;
 }
 
 Packet* FTPassElement::pull(int port) {
-    LOG("------------------------------");
-    LOG("Begin FTPassElement");
+    click_chatter("------------------------------");
+    click_chatter("Begin FTPassElement - pull");
+
+    Packet *p = input(port).pull();
+
+    const click_ether_vlan *vlan = reinterpret_cast<const click_ether_vlan *>(p->data());
+    VLANId vlan_id = (ntohs(vlan->ether_vlan_tci) & 0xFFF);
+
+    click_chatter("VLAN_ID: %d", vlan_id);
 
     //TODO: change port back to zero
-    LOG("End FTPassElement");
-    LOG("------------------------------");
+    click_chatter("End FTPassElement - pull");
+    click_chatter("------------------------------");
 
-    return input(port).pull();
-
-
+    return p;
 }
 
 CLICK_ENDDECLS

e3463a8027ce68de2b5f74e02bbd3282a3738dfc
Milad Ghaznavi <miladghaznavi@gmail.com>
2017-05-31 19:08:08 -0400

    The idea of having timestamp is implemented and tested. For local test, code is working! The test on physical environment is to be done!

--- FTPassElement.cc
+++ FTPassElement.cc
@@ -9,17 +9,7 @@ FTPassElement::FTPassElement() { }
 FTPassElement::~FTPassElement() { }
 
 void FTPassElement::push(int port, Packet *p) {
-    LOG("------------------------------");
-    LOG("Begin FTPassElement - push");
-
-    const click_ether_vlan *vlan = reinterpret_cast<const click_ether_vlan *>(p->data());
-    VLANId vlan_id = (ntohs(vlan->ether_vlan_tci) & 0xFFF);
-    LOG("Port: %d, VLAN_ID: %d", port, vlan_id);
-
-    LOG("End FTPassElement - push");
-    LOG("------------------------------");
-    //TODO: change port back to zero
-    output(port).push(p);
+    output(0).push(p);
 }
 
 CLICK_ENDDECLS

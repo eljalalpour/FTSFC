da0f34b980c96a7c6043b79e9e117fda4852041a
Milad Ghaznavi <miladghaznavi@gmail.com>
2017-05-31 23:17:14 -0400

    Adding some function for testing purposes

--- NFCounterMB.cc
+++ NFCounterMB.cc
@@ -16,29 +16,24 @@ Packet *NFCounterMB::simple_action(Packet *p) {
     LOG("Begin NFCounterMB %d:", _id);
 
     _counter++;
-    click_chatter("1");
+
+    click_chatter("counter: %u", _counter);
+
     WritablePacket* q1 = p->uniqueify();
     WritablePacket* q2 = p->uniqueify();
     WritablePacket* q3 = p->uniqueify();
     WritablePacket* q4 = p->uniqueify();
 
-    click_chatter("2");
     RandomState rs;
     FTPiggyBackMessage msg1 = rs.random_message(2, 1);
-    click_chatter("3");
-    WritablePacket* q5 = FTAppenderElement::encodeStates(p, msg1);
-    WritablePacket* q6 = FTAppenderElement::encodeStates(p, msg1);
-    WritablePacket* q7 = FTAppenderElement::encodeStates(p, msg1);
-    click_chatter("4");
+    WritablePacket* q5 = FTAppenderElement::encodeStates(q1, msg1);
+    WritablePacket* q6 = FTAppenderElement::encodeStates(q2, msg1);
+    WritablePacket* q7 = FTAppenderElement::encodeStates(q3, msg1);
 
     FTPiggyBackMessage msg2, msg3, msg4;
-    click_chatter("5");
-    FTAppenderElement::decodeStates(q5, msg2);
-    click_chatter("51");
-    FTAppenderElement::decodeStates(q6, msg3);
-    click_chatter("52");
-    FTAppenderElement::decodeStates(q7, msg4);
-    click_chatter("6");
+    WritablePacket* q8  = FTAppenderElement::encodeStates(q1, msg1);
+    WritablePacket* q9  = FTAppenderElement::encodeStates(q2, msg1);
+    WritablePacket* q10 = FTAppenderElement::encodeStates(q3, msg1);
 
     LOG("End NFCounterMB %d:", _id);
     LOG("--------------------");
--- non-local-test.click
+++ non-local-test.click
@@ -1,20 +1,8 @@
 require(package "FTSFC");
 
-FromDemp(vlan-tag-1.pcap)
-->VLANDecap
+FromDump(small-packets-1.pcap)
 ->CheckIPHeader(14)
 ->NFCounterMB(ID 0)
-->VLANEncap(VLAN_ID 6)
-->VLANEncap(VLAN_ID 6)
-
-->Queue
-
-->FTFilterElement(6)
-->CheckIPHeader(18)
-->VLANDecap
 ->CheckIPHeader(14)
 ->NFCounterMB(ID 1)
-->VLANEncap(VLAN_ID 7)
-->VLANEncap(VLAN_ID 7)
-->Queue
-->ToDevice(p0);
+->Discard;

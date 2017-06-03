38b4f137d0833fea7412c809d02d342895d17964
ejalalpo <el.jalalpour@gmail.com>
2017-06-02 20:11:02 -0400

    Compare file has been added

--- Compare.py
+++ Compare.py
@@ -22,19 +22,23 @@ def process(rate, count):
     count_dst= 0
     count_src = 0
     errorrate = 100
-    dst = "click " + "-e " + '\'require(package "FTSFC");FromDevice(p2)-> FTFilterElement(7)-> VLANDecap-> CheckIPHeader(14)-> FTLoggerElement(dst.%d.txt)-> ToDump(dst.pcap);\'' % (
+    dst = "click " + "-e " + '\'require(package "FTSFC");FromDevice(en1)-> FTFilterElement(7)-> VLANDecap-> CheckIPHeader(14)-> FTLoggerElement(dst.%d.txt)-> ToDump(dst.pcap);\'' % (
         rate)
     run_cmd(dst)
-    src = "click " + "-e " + '\'require(package "FTSFC");FTFastTCPGen(%d, %d, 60, 68:5b:35:c5:e9:39, 192.168.200.18,68:5b:35:c5:e9:55, 192.168.200.17, 100, 10)-> FTLoggerElement(src.%d.txt)-> VLANEncap(VLAN_ID 5)-> VLANEncap(VLAN_ID 5)-> ToDevice(p1);\'' % (
+    src = "click " + "-e " + '\'require(package "FTSFC");FTFastTCPGen(%d, %d, 60, 68:5b:35:c5:e9:39, 192.168.200.18,68:5b:35:c5:e9:55, 192.168.200.17, 100, 10)-> FTLoggerElement(src.%d.txt)-> VLANEncap(VLAN_ID 5)-> VLANEncap(VLAN_ID 5)-> ToDevice(en1);\'' % (
         rate, count, rate)
     run_cmd(src)
     if os.path.isfile(DST_PATH):
         count_dst = "wc " + "-l " + DST_PATH % (rate)
-        print(count_dst)
+        res = run_cmd(count_dst)
+        res = res.strip().split()[1]
+        print(res)
     if os.path.isfile(SRC_PATH):
         count_src = "wc " + "-l " + SRC_PATH % (rate)
-    #error = (int)count_src - (int)count_dst
-    error = 0.15
+        res2 = run_cmd(count_src)
+        res2 = res2.strip().split()[1]
+        print(res)
+    error = int(res2) - int(res)
     if count_src == 0:
         logger.error("devision to zero")
     else:

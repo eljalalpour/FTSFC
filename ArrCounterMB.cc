6ab55c9b41bb7ed1a6d923b1e97c274af4fc89b1
Milad Ghaznavi <miladghaznavi@gmail.com>
2017-06-15 22:32:38 -0400

    Fixing a bug in arr counter mbs

--- ArrCounterMB.cc
+++ ArrCounterMB.cc
@@ -18,11 +18,11 @@ Packet *ArrCounterMB::simple_action(Packet *p) {
     Router *r = this->router();
 
     //TODO: change the name of stateElement back to se
-//    FTStateElement *stateElement = (FTStateElement *)(r->find("se"));
-    stringstream ess;
-    ess << "se" << (int)_id;
-    DEBUG("state element: %s", ess.str().c_str());
-    FTStateElement *stateElement = (FTStateElement *)(r->find(ess.str().c_str()));
+    FTStateElement *stateElement = (FTStateElement *)(r->find("se"));
+//    stringstream ess;
+//    ess << "se" << (int)_id;
+//    DEBUG("state element: %s", ess.str().c_str());
+//    FTStateElement *stateElement = (FTStateElement *)(r->find(ess.str().c_str()));
 
     for (int i = 0; i < _counters.size(); ++i) {
         stringstream value;

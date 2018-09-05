701a25f9c6fdf2870dcb98333ea9987487bbdc57
miladghaznavi <miladghaznavi@gmail.com>
2018-09-05 13:07:09 -0400

    Non Fault Tolerant Counter MBs

---
+++ nf_lock_free_counter.cc
@@ -0,0 +1,34 @@
+#include "lock_free_counters.hh"
+#include <click/config.h>
+#include <click/router.hh>
+#include <clicknet/tcp.h>
+#include <click/args.hh>
+#include "nf_lock_free_counter.hh"
+
+CLICK_DECLS
+
+int NFLockFreeCounter::configure(Vector<String> &conf, ErrorHandler *errh) {
+    // set index param
+    Args(conf, this, errh).read_or_set("INDEX", _index, DEFAULT_INDEX);
+    LOG("NFLockFreeCounter index is %d!\n", _index);
+
+    return 0;
+}
+
+
+Packet *NFLockFreeCounter::simple_action(Packet *p) {
+    LOG("--------------------");
+    LOG("Begin NFLockFreeCounter with index %d:", _index);
+    Router *r = this->router();
+
+    LockFreeCounters *lfc = (LockFreeCounters *)(r->find("counters"));
+    ++lfc->counters[_index];
+
+    LOG("End NFLockFreeCounter %d:", _index);
+    LOG("--------------------");
+
+    return p;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(NFLockFreeCounter)
---
+++ nf_lock_free_counter.hh
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <click/config.h>
+#include <click/element.hh>
+#inculde <atomic>
+#inculde <vector>
+
+CLICK_DECLS
+
+#define DEFAULT_INDEX   0
+
+class NFLockFreeCounter : public Element {
+private:
+    int _index;
+
+public:
+    NFLockFreeCounter () : _index(DEFAULT_INDEX) { };
+
+    ~CounterNF() { };
+
+    const char *class_name() const { return "NFLockFreeCounter"; }
+
+    const char *port_count() const { return PORTS_1_1; }
+
+    const char *processing() const { return AGNOSTIC; }
+
+    Packet *simple_action(Packet *p);
+};
+
+CLICK_ENDDECLS

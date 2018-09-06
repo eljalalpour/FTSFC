faa9504fbcd2566c01bfe5f4295f7da678c4fedb
miladghaznavi <miladghaznavi@gmail.com>
2018-09-06 13:54:40 -0400

    Util Class

--- FTTypes.hh
+++ FTTypes.hh
@@ -58,8 +58,6 @@ typedef map<string, string> FTState;
 // FTModified shows if a variable has been modified
 typedef map<string, bool> FTModified;
 
-// Util functions
-unsigned long fast_random();
 
 class FTTimestampState {
 public:
@@ -260,5 +258,24 @@ public:
                         sr_pb_msg.pb_state(i).state().val(j);
             }//for
         }//for
+    }
+};
+
+class Utils {
+public:
+    static unsigned long fast_random() {
+        static unsigned long x=123456789, y=362436069, z=521288629;
+
+        unsigned long t;
+        x ^= x << 16;
+        x ^= x >> 5;
+        x ^= x << 1;
+
+        t = x;
+        x = y;
+        y = z;
+        z = t ^ x ^ y;
+
+        return z;
     }
 };

25c8622d399a91c35068d8a5d1afc659c389c1d7
miladghaznavi <miladghaznavi@gmail.com>
2018-09-06 09:50:15 -0400

    NAT !

--- FTTypes.cc
+++ FTTypes.cc
@@ -1,1 +1,17 @@
 #include "FTTypes.hh"
+
+unsigned long fast_random() {
+    static unsigned long x=123456789, y=362436069, z=521288629;
+
+    unsigned long t;
+    x ^= x << 16;
+    x ^= x >> 5;
+    x ^= x << 1;
+
+    t = x;
+    x = y;
+    y = z;
+    z = t ^ x ^ y;
+
+    return z;
+}
--- FTTypes.hh
+++ FTTypes.hh
@@ -59,21 +59,7 @@ typedef map<string, string> FTState;
 typedef map<string, bool> FTModified;
 
 // Util functions
-unsigned long fast_random() {
-    static unsigned long x=123456789, y=362436069, z=521288629;
-
-    unsigned long t;
-    x ^= x << 16;
-    x ^= x >> 5;
-    x ^= x << 1;
-
-    t = x;
-    x = y;
-    y = z;
-    z = t ^ x ^ y;
-
-    return z;
-}
+unsigned long fast_random();
 
 class FTTimestampState {
 public:

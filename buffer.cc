78f7a13cabedf7dcb0e356027a7c79edb9948a48
miladghaznavi <miladghaznavi@gmail.com>
2018-09-14 19:37:42 -0400

    Single thread

--- buffer.cc
+++ buffer.cc
@@ -54,7 +54,7 @@ void Buffer::push(int, Packet*p) {
 
     // Store the packet into buffer
 //    _packets[lts] = Packet::make(p->data(), p->length());
-    std::memset(p->data() + DEFAULT_OFFSET, 0, sizeof(PiggybackMessage));
+    memset(p->data() + DEFAULT_OFFSET, 0, sizeof(PiggybackMessage));
     _packets[lts] = Packet::make(p->data(), p->length());
 
     //TODO: find the best order, either first send the packet to Forwarder or release packets

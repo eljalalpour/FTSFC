c5161b7b232f7622528c03fe787132c005df2832
miladghaznavi <miladghaznavi@gmail.com>
2018-09-14 12:29:39 -0400

    Single queue

--- stats_collector.cc
+++ stats_collector.cc
@@ -72,7 +72,7 @@ void StatsCollector::collect() {
 
     for (auto i = 0; i < _buffer; ++i) {
         String buffer_name = "b" + i;
-        LOG("Finding %s!", buffer_name);
+        LOG("Finding %s!", buffer_name.c_str());
         auto _buffer_elm = (Buffer *) (r->find("buffer", buffer_name));
         _buffer_stats[i].push_back(_buffer_elm->length());
     }

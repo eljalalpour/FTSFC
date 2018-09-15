f2f1f8bd46a675b8d7f4f7394762823f871fe431
miladghaznavi <miladghaznavi@gmail.com>
2018-09-15 02:44:36 -0400

    Trivial FT

--- tf_lock_free_counter.cc
+++ tf_lock_free_counter.cc
@@ -31,13 +31,9 @@ Packet *TFLockFreeCounter::simple_action(Packet *p) {
     DEBUG("Begin TFLockFreeCounter");
 
     _init_transmitter();
-    LOG("After init");
 
-//    (*_trans).inoperation[_index] ++;
-    LOG("After increment");
-
+    (*_trans).inoperation[_index] ++;
     _trans->send();
-    LOG("After send");
 
     DEBUG("End TFLockFreeCounter");
     DEBUG("--------------------");

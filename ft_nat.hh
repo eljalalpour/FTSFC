2811e9148ceeffb3071b6d36838eeb8ae8664642
miladghaznavi <miladghaznavi@gmail.com>
2018-09-16 12:56:02 -0400

    fixing

--- ft_nat.hh
+++ ft_nat.hh
@@ -1,38 +1,39 @@
-#ifndef FTSFC_FT_NAT_HH
-#define FTSFC_FT_NAT_HH
+#pragma once
 
-//#include <unordered_map>
+#include <unordered_map>
 #include <click/config.h>
 #include <click/element.hh>
-//#include "shared_lock_free_state.hh"
+#include "shared_lock_free_state.hh"
 
 CLICK_DECLS
 
 class FTNAT : public Element {
 private:
     bool _init_state;
-//    SharedLockFreeState* _shared_state;
-//    std::unordered_map<uint32_t, bool> _fake_map;
+    SharedLockFreeState* _shared_state;
+    int _index;
+    std::unordered_map<uint32_t, bool> _fake_map;
 
-//    inline void _init_shared_state();
+    inline void _init_shared_state();
 
 public:
     FTNAT ();
 
     ~FTNAT();
 
-    const char *class_name() const { return "FTNAT"; }
+    const char *class_name() const { return "FTNAT "; }
 
     const char *port_count() const { return PORTS_1_1; }
 
     const char *processing() const { return AGNOSTIC; }
 
+    int configure(Vector<String> &conf, ErrorHandler *errh);
+
     Packet *simple_action(Packet *p);
 
-//    inline bool bad_header(const click_ip *iph);
-//
-//    inline uint32_t flow_id(Packet *p);
+    inline bool bad_header(const click_ip *iph);
+
+    inline uint32_t flow_id(Packet *p);
 };
 
 CLICK_ENDDECLS
-#endif

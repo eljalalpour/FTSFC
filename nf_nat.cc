af1cd3ba64fdc3160f35a4a8507df59ca56d4103
miladghaznavi <miladghaznavi@gmail.com>
2018-09-16 15:36:50 -0400

    real chain

---
+++ nf_nat.cc
@@ -0,0 +1,71 @@
+#include "lock_free_array.hh"
+#include "defs.hh"
+#include <click/config.h>
+#include <click/router.hh>
+#include <click/args.hh>
+#include <click/ipflowid.hh>
+#include "nf_nat.hh"
+
+CLICK_DECLS
+
+NFNAT::NFNAT () { };
+
+NFNAT::~NFNAT() { };
+
+bool NFNAT::bad_header(const click_ip *iph) {
+    return (iph->ip_p != IP_PROTO_TCP && iph->ip_p != IP_PROTO_UDP)
+           || !IP_FIRSTFRAG(iph);
+}
+
+uint32_t NFNAT::flow_id(Packet *p) {
+    // Simulating Real NAT stuff!
+    IPFlowID flow_id(p);
+    uint64_t ip_part =
+            (uint64_t)flow_id.saddr().addr() |
+            ((uint64_t)flow_id.daddr().addr() << 32);
+
+    uint32_t pr_part =
+            (uint64_t)flow_id.sport() |
+            ((uint64_t)flow_id.dport()) << 16;
+
+    std::hash<uint64_t> hasher;
+    uint32_t hash_val =
+            ((uint32_t)hasher(ip_part) % (1 << 16)) |
+            pr_part;
+
+    auto it = _fake_map.find(hash_val);
+    if (it == _fake_map.end())
+        _fake_map[hash_val] = 1;
+
+    // Just return some random value!
+    return hash_val % STATE_LEN;
+}
+
+Packet *NFNAT::simple_action(Packet *p) {
+    DEBUG("--------------------");
+    DEBUG("Begin NFNAT");
+    Router *r = this->router();
+
+    const click_ip *iph = p->ip_header();
+
+    if (bad_header(iph)) {
+        p->kill();
+        return 0;
+    }//if
+
+    // Finding flow id
+    uint32_t fid = flow_id(p);
+
+    // Keep as is
+    LockFreeArray *afc = (LockFreeArray *)(r->find("array"));
+    if (afc->array[fid] == 0)
+        afc->array[fid] = 1;
+
+    DEBUG("End NFNAT");
+    DEBUG("--------------------");
+
+    return p;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(NFNAT)
---
+++ nf_nat.hh
@@ -0,0 +1,33 @@
+#ifndef NF_NAT_HH
+#define NF_NAT_HH
+
+#include <unordered_map>
+#include <click/config.h>
+#include <click/element.hh>
+
+CLICK_DECLS
+
+class NFNAT : public Element {
+private:
+    std::unordered_map<uint32_t, bool> _fake_map;
+
+public:
+    NFNAT ();
+
+    ~NFNAT();
+
+    const char *class_name() const { return "NFNAT"; }
+
+    const char *port_count() const { return PORTS_1_1; }
+
+    const char *processing() const { return AGNOSTIC; }
+
+    Packet *simple_action(Packet *p);
+
+    inline bool bad_header(const click_ip *iph);
+
+    inline uint32_t flow_id(Packet *p);
+};
+
+CLICK_ENDDECLS
+#endif

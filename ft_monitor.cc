b478518755bb08c91b94ce353b23828d8a730803
miladghaznavi <miladghaznavi@gmail.com>
2018-09-16 13:43:03 -0400

    real chain configs

---
+++ ft_monitor.cc
@@ -0,0 +1,51 @@
+#include <click/config.h>
+#include <click/router.hh>
+#include <click/args.hh>
+#include "ft_lock_free_counter.hh"
+
+CLICK_DECLS
+
+FTMonitor::FTMonitor () : _init_state(false) { };
+
+FTMonitor::~FTMonitor() { };
+
+void FTMonitor::_init_shared_state() {
+    if (!_init_state) {
+        Router *r = this->router();
+        _shared_state = (SharedLockFreeState *)(r->find("shared_state"));
+        _init_state = true;
+    }//if
+}
+
+int FTMonitor::configure(Vector<String> &conf, ErrorHandler *errh) {
+    // set index param
+    if (Args(conf, this, errh)
+                .read("INDEX", _index)
+                .complete() < 0)
+        return -1;
+
+    DEBUG("FTMonitor index is %d!\n", _index);
+
+    return 0;
+}
+
+Packet *FTMonitor::simple_action(Packet *p) {
+    DEBUG("--------------------");
+    DEBUG("Begin FTMonitor with index %d:", _index);
+
+    _init_shared_state();
+
+    ++_shared_state->inoperation[_index];
+
+    for (int i = 0; i < FOR_COUNT; ++i) {
+        rand();
+    }//for
+
+    DEBUG("End FTMonitor %d:", _index);
+    DEBUG("--------------------");
+
+    return p;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(FTMonitor)
---
+++ ft_monitor.hh
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "defs.hh"
+#include <click/config.h>
+#include <click/element.hh>
+#include "shared_lock_free_state.hh"
+
+CLICK_DECLS
+
+#define DEFAULT_INDEX 0
+#define FOR_COUNT     0
+
+
+class FTMonitor : public Element {
+private:
+    bool _init_state;
+    SharedLockFreeState* _shared_state;
+    int _index;
+
+    inline void _init_shared_state();
+
+public:
+    FTMonitor ();
+
+    ~FTMonitor();
+
+    const char *class_name() const { return "FTMonitor"; }
+
+    const char *port_count() const { return PORTS_1_1; }
+
+    const char *processing() const { return AGNOSTIC; }
+
+    int configure(Vector<String> &conf, ErrorHandler *errh);
+
+    Packet *simple_action(Packet *p);
+};
+
+CLICK_ENDDECLS

c9a5992161d195791de0c57e79686548df8f80d9
miladghaznavi <miladghaznavi@gmail.com>
2018-09-17 01:48:03 -0400

    Receive ack

--- client.hh
+++ client.hh
@@ -31,9 +31,9 @@ private:
         write(scp->socket, CAST_TO_BYTES(scp->state), sizeof(State));
 
 //        // Wait for the response
-//        char c;
-//        read(scp->socket, &c, sizeof(char));
-//        DEBUG("Read from socket: %c", c);
+        char c;
+        read(scp->socket, &c, sizeof(char));
+        DEBUG("Read from socket: %c", c);
 
         return 0;
     }

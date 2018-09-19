b68c6415109c1855032a582be9aaff20094c4973
miladghaznavi <miladghaznavi@gmail.com>
2018-09-19 16:36:43 -0400

    FT Simulator

--- ftmb_sim.cc
+++ ftmb_sim.cc
@@ -16,6 +16,12 @@ int FTMBSim::configure(Vector<String> &conf, ErrorHandler *errh) {
                 .read("DELAY", _delay)
                 .complete() < 0)
         return -1;
+
+    // Config parameters are in ms, convert them to us
+    _period *= MS2US;
+    _delay *= MS2US;
+    _first_packet_seen = false;
+
     DEBUG("FTMBSim period is %d, delay is %d!\n", _period, _delay);
 
     return 0;
@@ -25,7 +31,17 @@ Packet *FTMBSim::simple_action(Packet *p) {
     DEBUG("--------------------");
     DEBUG("Begin FTMBSim");
 
-    // TODO: implement periodic delay
+    if (!_first_packet_seen) {
+        _first_packet_seen = true;
+        _last_snapshot_timestamp = Timestamp::now();
+    }//if
+
+    auto now = Timestamp::now();
+    auto passed = (now - _last_snapshot_timestamp).usec();
+    if (passed >= _period) {
+        usleep(_delay);
+        _last_snapshot_timestamp = now;
+    }//if
 
     DEBUG("End FTMBSim");
     DEBUG("--------------------");
--- ftmb_sim.hh
+++ ftmb_sim.hh
@@ -3,15 +3,19 @@
 #include "defs.hh"
 #include <click/config.h>
 #include <click/element.hh>
+#include <click/timestamp.hh>
 
 CLICK_DECLS
 
-#define DEFAULT_INDEX   0
+#define MS2US   1000
 
 class FTMBSim : public Element {
 private:
     int _period;
     int _delay;
+    bool _first_packet_seen;
+
+    Timestamp _last_snapshot_timestamp;
 
 public:
     FTMBSim ();

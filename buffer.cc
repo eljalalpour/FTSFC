256edf82ad192117f5e77ab4105d52b801aaefb2
miladghaznavi <miladghaznavi@gmail.com>
2018-09-19 20:56:13 -0400

    Less data to forwarder

--- buffer.cc
+++ buffer.cc
@@ -44,7 +44,10 @@ void Buffer::push(int, Packet*p) {
     DEBUG("Begin Buffer");
 
 //    output(TO_FORWARDER).push(p->clone());
-    output(TO_FORWARDER).push(Packet::make(p->data(), DEFAULT_OFFSET + sizeof(PiggybackMessage)));
+    Packet* q = p->clone();
+    q->take(p->length() - (DEFAULT_OFFSET + sizeof(PiggybackMessage)));
+    output(TO_FORWARDER).push(q);
+
 
     // Cast the piggyback message and extract the timestamps
     PiggybackMessage* _msg = CAST_PACKET_TO_PIGGY_BACK_MESSAGE(p);

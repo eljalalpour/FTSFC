fe24a7a9f2d23f40c2996bf591a7d72f47e6393c
miladghaznavi <miladghaznavi@gmail.com>
2018-12-21 03:56:56 -0500

    For chain length and FT

---
+++ sig-conf/clickgen.py
@@ -0,0 +1,474 @@
+import os.path
+import sys
+import argparse
+import os.path
+from defs import *
+
+def def_parser():
+    parser = argparse.ArgumentParser(description="Generating click configurations")
+    parser.add_argument('-c',
+                        '--chain',
+                        dest='c',
+                        help='chain size',
+                        type=int,
+                        required=True)
+    parser.add_argument('-f',
+                        '--fail',
+                        dest='f',
+                        help='Number of failures',
+                        type=int,
+                        default=DEFAULT_FAILS)
+    parser.add_argument('-t',
+                        '--thread',
+                        dest='t',
+                        help='Number of threads',
+                        type=int,
+                        default=DEFAULT_THREADS)
+    parser.add_argument('-b',
+                        '--batch',
+                        dest='b',
+                        help='Buffer batch size',
+                        type=int,
+                        default=DEFAULT_BATCH)
+    parser.add_argument('-o',
+                        '--output',
+                        dest='o',
+                        help='output folder',
+                        type=str,
+                        required=True)
+
+    return parser
+
+
+def parse_args(parser):
+    opts = vars(parser.parse_args(sys.argv[1:]))
+
+    if not os.path.isdir(opts['o']):
+        raise ValueError("Output folder '{}' does not exist!".format(opts['i']))
+
+    return opts
+
+
+def shared_state_declare(ch_len, chain_pos, f):
+    return SHARED_STATE_FORMAT_STR.format(**{
+        CHAIN: ch_len,
+        ID: chain_pos,
+        F: f,
+    })
+
+
+def dev_name_list(from_or_to, device, thrds):
+    """
+    list of device element names
+    :param from_or_to: FromDevice or ToDevice
+    :param device: the exact device id
+    :param thrds: a number denoting the number of threads
+    :return: list of names
+    """
+    result = []
+    format_str = ''
+    if from_or_to == 'from':
+        format_str = FROM_DEVICE_NAME_FORMAT_STR
+    elif from_or_to == 'to':
+        format_str = TO_DEVICE_NAME_FORMAT_STR
+
+    for queue in range(thrds):
+        result.append(
+            format_str.format(**{
+                DEVICE: device,
+                QUEUE: queue
+            })
+        )
+    return result
+
+
+def fd_data_names_list(thrds):
+    """
+    list of data FromDevice element names
+    :param thrds: a number denoting the number of threads
+    :return: list of names
+    """
+    return dev_name_list('from', DATA_DEVICE_ID, thrds)
+
+
+def fd_state_names_list(thrds):
+    """
+    list of state FromDevice element names
+    :param thrds: a number denoting the number of threads
+    :return: list of names
+    """
+    return dev_name_list('from', STATE_DEVICE_ID, thrds)
+
+
+def fd_names_list(chain_pos, thrds):
+    """
+    list of FromDevice element names based on the position of middlebox in the chain
+    and the number of threads
+    :param chain_pos: a number denoting the position of middlebox in the chain
+    :param thrds: a number denoting the number of threads
+    :return: the list of device names
+    """
+    name_list = fd_data_names_list(thrds)
+
+    if chain_pos == 0:
+        name_list.extend(fd_state_names_list(thrds))
+
+    return name_list
+
+
+def td_data_names_list(thrds):
+    """
+    list of data ToDevice element names
+    :param thrds: a number denoting the number of threads
+    :return: list of names
+    """
+    return dev_name_list('to', DATA_DEVICE_ID, thrds)
+
+
+def td_state_names_list(thrds):
+    """
+    list of state FromDevice element names
+    :param thrds: a number denoting the number of threads
+    :return: list of names
+    """
+    return dev_name_list('to', STATE_DEVICE_ID, thrds)
+
+
+def td_names_list(chain_pos, thrds):
+    """
+    list of FromDevice element names based on the position of middlebox in the chain
+    and the number of threads
+    :param chain_pos: a number denoting the position of middlebox in the chain
+    :param thrds: a number denoting the number of threads
+    :return: the list of device names
+    """
+    name_list = td_data_names_list(thrds)
+
+    if chain_pos == 0:
+        name_list.extend(td_state_names_list(thrds))
+
+    return name_list
+
+
+def dev_declares(from_or_to, chain_pos, thrds):
+    """
+    format device declares based on the position of middlebox in the chain
+    and the number of threads
+    :param from_or_to: FromDevice or ToDevice
+    :param chain_pos: a number denoting the position of middlebox in the chain
+    :param thrds: a number denoting the number of threads
+    :return: formatted string of device declares
+    """
+    def inner_dev_list(dev_format_str, device, thrds):
+        """
+        Helper function for multiple threads
+        :param dev_format_str: device format string
+        :param device: the exact device id
+        :param thrds: a number denoting the number of threads
+        :return: list of device declares
+        """
+        result = []
+        for queue in range(thrds):
+            result.append(
+                dev_format_str.format(**{
+                    DEVICE: device,
+                    QUEUE: queue
+                })
+            )
+        return result
+
+    # If the middlebox at the beginning of the chain, then
+    # it receives traffic from two NICs
+
+    if from_or_to == 'from':
+        dev_format_str = FROM_DEVICE_FORMAT_STR
+    else:
+        dev_format_str = TO_DEVICE_FORMAT_STR
+
+    devs = inner_dev_list(dev_format_str, DATA_DEVICE_ID, thrds)
+
+    if (chain_pos == 0 and from_or_to == 'from') or \
+            (chain_pos == -1) and from_or_to == 'to':
+        devs.extend(inner_dev_list(dev_format_str, STATE_DEVICE_ID, thrds))
+
+    return '\n'.join(devs)
+
+
+def from_dev_declares(chain_pos, thrds):
+    """
+    format from device declares based on the position of middlebox in the chain
+    and the number of threads
+    :param chain_pos: a number denoting the position of middlebox in the chain, use -1 for the last middlebox
+    :param thrds: a number denoting the number of threads
+    :return: formatted string of from device declares
+    """
+    return dev_declares('from', chain_pos, thrds)
+
+
+def to_dev_declares(chain_pos, thrds):
+    """
+    format to device declares based on the position of middlebox in the chain
+    and the number of threads
+    :param chain_pos: a number denoting the position of middlebox in the chain
+    :param thrds: a number denoting the number of threads
+    :return: formatted string of the to device declares
+    """
+    return dev_declares('to', chain_pos, thrds)
+
+
+def thread_sched_declare(from_devs_list):
+    """
+    Format thread schedule declares from the list of FromDeviceDeclares
+    :param from_devs_list:
+    :return: formatted string of the thread schedule
+    """
+    result = []
+    for i in range(len(from_devs_list)):
+        result.append("{} {}".format(from_devs_list[i], i))
+
+    return THREAD_SCHED_FORMAT_STR.format(', '.join(result))
+
+
+def ft_block_names_list(thrds):
+    """
+    the list of FT block names
+    :param thrds: a number denoting the number of threads
+    :return: names list
+    """
+    return [FT_BLOCK_NAME_FORMAT_STR.format(i) for i in range(thrds)]
+
+
+def dev_ip(chain_pos, _40_or_10):
+    """
+    IP address of the device based on the given position of middlebox in the chain
+    :param chain_pos: a number denoting the position of middlebox in the chain
+    :param _40_or_10: which NIC, 10 or 40
+    :return: device ip
+    """
+    result = ''
+    if _40_or_10 == '40':
+        result = NIC_40G_IP_LIST[chain_pos + FIRST_AQUA_MACHINE_IN_CHAIN]
+    elif _40_or_10 == '10':
+        result = NIC_10G_IP_LIST[chain_pos + FIRST_AQUA_MACHINE_IN_CHAIN]
+
+    return result
+
+
+def dev_mac(chain_pos, _40_or_10):
+    """
+    IP address of the device based on the given position of middlebox in the chain
+    :param chain_pos: a number denoting the position of middlebox in the chain
+    :param _40_or_10: which NIC, 10 or 40
+    :return: device ip
+    """
+    result = ''
+    if _40_or_10 == '40':
+        result = NIC_40G_MAC_LIST[chain_pos + FIRST_AQUA_MACHINE_IN_CHAIN]
+    elif _40_or_10 == '10':
+        result = NIC_10G_MAC_LIST[chain_pos + FIRST_AQUA_MACHINE_IN_CHAIN]
+
+    return result
+
+
+def src_ip_filter(chain_pos, thrd=0):
+    return "1.{}.{}.{}".format(
+        chain_pos + 1,
+        thrd + 1,
+        thrd + 1,
+    )
+
+
+def gen_mb_params_str(thrd):
+    return thrd
+
+
+def ft_blocks_declares(chain_pos, thrds):
+    """
+    format ft block declares based on the position of middlebox in the chain, number of threads, and
+    mb parameters
+    :param chain_pos: a number denoting the position of middlebox in the chain
+    :param thrds: a number denoting the number of threads
+    :return: formatted string of the ft_blocks
+    """
+
+    declares = []
+    format_str = FT_BLOCK_FORMAT_STR
+    if chain_pos == -1:
+        format_str = FT_BLOCK_WITH_BUFFER_FORMAT_STR
+    for i in range(thrds):
+        mb_p = gen_mb_params_str(i)
+        if mb_p != '':
+            mb_p = ', ' + str(mb_p)
+
+        params = {
+            QUEUE: i,
+            ID: chain_pos,
+            DATA_SRC_IP: src_ip_filter(chain_pos, i),
+            MB_PARAMS: mb_p,
+        }
+        if chain_pos == -1:
+            params[STATE_SRC_IP] = dev_ip(chain_pos, '10')
+
+        declares.append(
+            format_str.format(**params)
+        )
+
+    return '\n'.join(declares)
+
+
+def links(chain_pos, thrds):
+    """
+    format link strings, each link is 'FromDevice... -> FTBlock -> ...ToDevice'
+    :param chain_pos: a number denoting the position of middlebox in the chain
+    :param thrds: a number denoting the number of threads
+    :return: formatted string of links
+    """
+    fd_data_names = fd_data_names_list(thrds)
+    td_data_names = td_data_names_list(thrds)
+    fd_state_names = fd_state_names_list(thrds)
+    td_state_names = td_state_names_list(thrds)
+    ft_block_names = ft_block_names_list(thrds)
+
+    format_str = LINK_FORMAT_STR
+    if chain_pos == 0:
+        format_str = LINK_WITH_FORWARDER_FORMAT_STR
+    elif chain_pos == -1:
+        format_str = LINK_WITH_BUFFER_FORMAT_STR
+
+    ll = []
+    for i in range(thrds):
+        params = {
+            QUEUE: i,
+            'FROM_DATA_DEVICE_NAME': fd_data_names[i],
+            'FT_BLOCK_NAME': ft_block_names[i],
+            'TO_DATA_DEVICE_NAME': td_data_names[i],
+        }
+        if chain_pos == 0:
+            params['FROM_STATE_DEVICE_NAME'] = fd_state_names[i]
+        elif chain_pos == -1:
+            params['TO_STATE_DEVICE_NAME'] = td_state_names[i]
+
+        ll.append(format_str.format(**params))
+
+    return '\n'.join(ll)
+
+
+def ft_block_def(chain_pos, ch_len, batch, mb_params):
+    """
+    format a block declare
+    :param chain_pos: a number denoting the position of middlebox in the chain
+    :param ch_len:
+    :param batch:
+    :param mb_params: the list of parameter lists for MB's threads
+    :return: formatted string of ft block
+    """
+    data_dev = '40'
+    state_dev = '10'
+
+    mb_params_str = ''
+    if mb_params is not None and len(mb_params) > 0:
+        mb_params_str = ', ' + ', '.join(mb_params)
+
+    if chain_pos == 0:
+        result = FT_BLOCK_WITH_FORWARDER.format(**{
+            CHAIN: ch_len,
+
+            MB_PARAM: mb_params_str,
+
+            DATA_SRC_MAC: dev_mac(chain_pos, data_dev),
+            DATA_DST_MAC: dev_mac(chain_pos + 1, data_dev),
+            DATA_DST_IP: dev_ip(chain_pos + 1, data_dev),
+        })
+
+    elif chain_pos == -1 or chain_pos == (ch_len - 1):
+        result = FT_BLOCK_WITH_BUFFER.format(**{
+            BATCH: batch,
+            CHAIN: ch_len,
+
+            MB_PARAM: mb_params_str,
+
+            SRC_IP_FILTER: src_ip_filter(ch_len - 2),
+
+            DATA_SRC_MAC: dev_mac(ch_len - 1, data_dev),
+            DATA_DST_MAC: dev_mac(-FIRST_AQUA_MACHINE_IN_CHAIN, data_dev),
+            DATA_SRC_IP: dev_ip(ch_len - 1, data_dev),
+            DATA_DST_IP: dev_ip(-FIRST_AQUA_MACHINE_IN_CHAIN, data_dev),
+
+            STATE_SRC_MAC: dev_mac(ch_len - 1, state_dev),
+            STATE_DST_MAC: dev_mac(0, state_dev),
+            STATE_SRC_IP: dev_ip(ch_len - 1, state_dev),
+            STATE_DST_IP: dev_ip(0, state_dev),
+        })
+
+    else:
+        result = FT_BLOCK.format(**{
+            SRC_IP_FILTER: src_ip_filter(chain_pos - 1),
+
+            MB_PARAM: mb_params_str,
+
+            DATA_SRC_MAC: dev_mac(chain_pos, data_dev),
+            DATA_DST_MAC: dev_mac(chain_pos + 1, data_dev),
+            DATA_DST_IP: dev_ip(chain_pos + 1, data_dev),
+        })
+
+    return result
+
+
+def ftc_click(ch_len, chain_pos, f, thrds, batch):
+    """
+    Click code for FTC
+    :return: Click code in string
+    """
+    string_map = {
+        SHARED_STATE_DECLARE: shared_state_declare(ch_len,
+                                                   chain_pos,
+                                                   f),
+
+        FT_BLOCK_DEF: ft_block_def(chain_pos,
+                                   ch_len,
+                                   batch,
+                                   COUNTER_MB_PARAMS),
+
+        FROM_DEVICE_DECLARES: from_dev_declares(chain_pos,
+                                                thrds),
+
+        TO_DEVICE_DECLARES: to_dev_declares(chain_pos,
+                                            thrds),
+
+        THREAD_SCHEDULES: thread_sched_declare(fd_names_list(chain_pos,
+                                                             thrds)),
+
+        FT_BLOCKS_DECLARES: ft_blocks_declares(chain_pos,
+                                               thrds),
+
+        LINKS: links(chain_pos, thrds),
+    }
+
+    return FT.format(**string_map)
+
+
+def generate(ch_len, f, thrds, batch):
+    clicks = []
+    for chain_pos in range(ch_len - 1):
+        clicks.append(ftc_click(ch_len, chain_pos, f, thrds, batch))
+
+    clicks.append(ftc_click(ch_len, -1, f, thrds, batch))
+
+    return clicks
+
+
+def store(output_path, clicks):
+    for i in range(len(clicks)):
+        file_name = os.path.join(output_path, FTC_OUTPUT_NAME_FORMAT_STR.format(i + 1))
+        with open(file_name, 'w') as handle:
+            handle.write(clicks[i])
+
+
+def main():
+    opts = parse_args(def_parser())
+    clicks = generate(opts['c'], opts['f'], opts['t'], opts['b'])
+    store(opts['o'], clicks)
+
+
+if __name__ == "__main__":
+    main()
---
+++ sig-conf/defs.py
@@ -0,0 +1,215 @@
+FT = """
+// Shared state
+{SHARED_STATE_DECLARE}
+
+// FT Block definition
+{FT_BLOCK_DEF}
+
+// From device declarations
+{FROM_DEVICE_DECLARES}
+
+// To device declarations
+{TO_DEVICE_DECLARES}
+
+// Scheduling
+{THREAD_SCHEDULES}
+
+// FT Block declarations
+{FT_BLOCKS_DECLARES}
+
+// MB graph
+{LINKS}
+"""
+
+FT_BLOCK = """elementclass FTBlock {{
+$id, $src_ip{MB_PARAM}|
+    input
+    -> MarkIPHeader(14)
+    -> IPFilter(allow udp && src {SRC_IP_FILTER}/16)
+    -> PMProcess
+    -> FTLockFreeCounter(INDEX $index)
+    -> PMConstruct(ID $id)
+    -> MarkIPHeader(14)
+    -> StoreEtherAddress({DATA_SRC_MAC}, src)
+    -> StoreEtherAddress({DATA_DST_MAC}, dst)
+    -> StoreIPAddress($src_ip, src)
+    -> StoreIPAddress({DATA_DST_IP}, dst)
+    -> output;
+}}
+"""
+
+FT_BLOCK_WITH_FORWARDER = """elementclass FTBlock {{
+$id, $src_ip{MB_PARAM} |
+    forwarder::Forwarder(CHAIN {CHAIN});
+
+    input[0]
+    -> MarkIPHeader(14)
+    -> IPFilter(allow udp && src 2.0.0.0/16)
+    -> [1]forwarder;
+
+    input[1]
+    -> MarkIPHeader(14)
+    -> IPFilter(allow udp && src 1.0.0.0/16)
+    -> forwarder
+    -> PMProcess
+    -> FTLockFreeCounter(INDEX $index)
+    -> PMConstruct(ID $id)
+    -> MarkIPHeader(14)
+    -> StoreIPAddress($src_ip, src)
+    -> StoreEtherAddress({DATA_SRC_MAC}, src)
+    -> StoreEtherAddress({DATA_DST_MAC}, dst)
+    -> StoreIPAddress({DATA_DST_IP}, dst)
+    -> output;
+}}
+"""
+
+FT_BLOCK_WITH_BUFFER = """elementclass FTBlock {{
+$id, $DATA_SRC_IP, $STATE_SRC_IP{MB_PARAM} |
+    buffer::Buffer(BATCH {BATCH}, CHAIN {CHAIN});
+
+    fd1
+    -> MarkIPHeader(14)
+    -> IPFilter(allow udp && src {SRC_IP_FILTER}/16)
+    -> PMProcess
+    -> FTLockFreeCounter(INDEX $index)
+    -> PMConstruct(ID $id)
+    -> buffer;
+
+    // To the outside world
+    buffer[0]
+    -> MarkIPHeader(14)
+    -> StoreEtherAddress({DATA_SRC_MAC}, src)
+    -> StoreEtherAddress({DATA_DST_MAC}, dst)
+    -> StoreIPAddress($DATA_SRC_IP, src)
+    -> StoreIPAddress({DATA_DST_IP}, dst)
+    -> [0]output;
+
+    // To the forwarder
+    buffer[1]
+    -> MarkIPHeader(14)
+    -> StoreEtherAddress({STATE_SRC_MAC}, src)
+    -> StoreEtherAddress({STATE_DST_MAC}, dst)
+    -> StoreIPAddress($STATE_SRC_IP, src)
+    -> StoreIPAddress({STATE_DST_IP}, dst)
+    -> [1]output;
+}}
+"""
+
+LINK_FORMAT_STR = """// Queue {QUEUE}
+{FROM_DATA_DEVICE_NAME}
+-> {FT_BLOCK_NAME}
+-> {TO_DATA_DEVICE_NAME}
+"""
+
+LINK_WITH_FORWARDER_FORMAT_STR = """// Queue {QUEUE}
+{FROM_DATA_DEVICE_NAME}
+-> [0]{FT_BLOCK_NAME};
+
+{FROM_STATE_DEVICE_NAME}
+-> [1]{FT_BLOCK_NAME};
+
+{FT_BLOCK_NAME}
+-> {TO_DATA_DEVICE_NAME}
+"""
+
+LINK_WITH_BUFFER_FORMAT_STR = """// Queue {QUEUE}
+{FROM_DATA_DEVICE_NAME}
+-> {FT_BLOCK_NAME};
+
+{FT_BLOCK_NAME}[0]
+-> {TO_DATA_DEVICE_NAME};
+
+{FT_BLOCK_NAME}[1]
+-> {TO_STATE_DEVICE_NAME};
+"""
+
+SHARED_STATE_FORMAT_STR = "shared_state::SharedLockFreeState(CHAIN {CHAIN}, ID {ID}, F {F})"
+
+FROM_DEVICE_NAME_FORMAT_STR = "fd_{DEVICE}_{QUEUE}"
+THREAD_SCHED_FORMAT_STR = "StaticThreadSched({});"
+FROM_DEVICE_FORMAT_STR = "fd_{DEVICE}_{QUEUE}::FromDPDKDevice({DEVICE}, {QUEUE});"
+
+TO_DEVICE_NAME_FORMAT_STR = "td_{DEVICE}_{QUEUE}"
+TO_DEVICE_FORMAT_STR = "td_{DEVICE}_{QUEUE}::ToDPDKDevice({DEVICE}, {QUEUE});"
+
+FT_BLOCK_NAME_FORMAT_STR = "ftb_{}"
+FT_BLOCK_FORMAT_STR = "ftb_{QUEUE}::FTBlock({ID}, {DATA_SRC_IP}{MB_PARAMS});"
+FT_BLOCK_WITH_BUFFER_FORMAT_STR = "ftb_{QUEUE}::FTBlock({ID}, {DATA_SRC_IP}, " \
+                                  "{STATE_SRC_IP}{MB_PARAMS});"
+
+ID = "ID"
+F = "F"
+CHAIN = "CHAIN"
+MB_PARAM = 'MB_PARAM'
+
+DATA_SRC_IP = "DATA_SRC_IP"
+DATA_DST_IP = "DATA_DST_IP"
+DATA_SRC_MAC = "DATA_SRC_MAC"
+DATA_DST_MAC = "DATA_DST_MAC"
+
+STATE_SRC_IP = "STATE_SRC_IP"
+STATE_DST_IP = "STATE_DST_IP"
+STATE_SRC_MAC = "STATE_SRC_MAC"
+STATE_DST_MAC = "STATE_DST_MAC"
+
+DEVICE = "DEVICE"
+QUEUE = "QUEUE"
+MB_PARAMS = "MB_PARAMS"
+
+BATCH = "BATCH"
+SRC_IP_FILTER = "SRC_IP_FILTER"
+
+SHARED_STATE_DECLARE = "SHARED_STATE_DECLARE"
+FT_BLOCK_DEF = "FT_BLOCK_DEF"
+FROM_DEVICE_DECLARES = "FROM_DEVICE_DECLARES"
+TO_DEVICE_DECLARES = "TO_DEVICE_DECLARES"
+THREAD_SCHEDULES = "THREAD_SCHEDULES"
+FT_BLOCKS_DECLARES = "FT_BLOCKS_DECLARES"
+LINKS = "LINKS"
+
+AQUA_MACHINES = ["aqua{:02d}".format(s) for s in range(1, 13)]
+NIC_10G_IP_LIST = ["192.168.1.1{:02d}".format(s) for s in range(1, 13)]
+NIC_10G_MAC_LIST = [
+    '0c:c4:7a:73:fa:72',
+    '0c:c4:7a:73:fa:dc',
+    '0c:c4:7a:73:fa:26',
+    '0c:c4:7a:73:fa:20',
+    '0c:c4:7a:73:fa:6e',
+    '',  # Aqua06 is out of order
+    '0c:c4:7a:73:fa:54',
+    '0c:c4:7a:73:fa:6a',
+    '0c:c4:7a:73:f9:ec',
+    '0c:c4:7a:73:fa:18',
+    '0c:c4:7a:73:fa:e6',
+    '0c:c4:7a:73:fa:9e',
+]
+
+NIC_40G_IP_LIST = ["10.70.0.{}".format(s) for s in range(1, 13)]
+NIC_40G_MAC_LIST = [
+    'f4:52:14:69:a5:20',
+    'e4:1d:2d:13:9c:30',
+    'e4:1d:2d:13:9f:80',
+    'e4:1d:2d:13:9f:e0',
+    'e4:1d:2d:13:9e:b0',
+    '',  # Aqua06 is out of order
+    'e4:1d:2d:13:9e:d0',
+    'f4:52:14:5a:90:70',
+    'e4:1d:2d:13:9c:60',
+    'e4:1d:2d:0c:89:e0',
+    'e4:1d:2d:13:9e:e0',
+    'e4:1d:2d:0c:89:d0',
+]
+
+STATE_DEVICE_ID = 0
+DATA_DEVICE_ID = 1
+
+FIRST_AQUA_MACHINE_IN_CHAIN = 6
+
+COUNTER = 'counter'
+COUNTER_MB = 'FTLockFreeCounter(INDEX $index)'
+COUNTER_MB_PARAMS = ["$index"]
+
+FTC_OUTPUT_NAME_FORMAT_STR = 'ft-{}.click'
+DEFAULT_FAILS = 1
+DEFAULT_THREADS = 1
+DEFAULT_BATCH = 5

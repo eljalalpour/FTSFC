672ea3b0f919d59039fb2ba074162248c68155f6
miladghaznavi <miladghaznavi@gmail.com>
2018-12-31 18:23:33 -0500

    IPRewriterBase

---
+++ rewriterbase.cc
@@ -0,0 +1,471 @@
+
+#include <click/config.h>
+#include "rewriterbase.hh"
+#include "../../elements/ip/iprwpatterns.hh"
+#include "../../elements/ip/iprwmapping.hh"
+#include "../../elements/ip/iprwpattern.hh"
+#include <clicknet/ip.h>
+#include <clicknet/tcp.h>
+#include <clicknet/udp.h>
+#include <click/llrpc.h>
+#include <click/args.hh>
+#include <click/straccum.hh>
+#include <click/error.hh>
+#include <click/algorithm.hh>
+#include <click/heap.hh>
+
+CLICK_DECLS
+
+//
+// Mapper
+//
+
+void
+Mapper::notify_rewriter(RewriterBase *, RewriterInput *, ErrorHandler *)
+{
+}
+
+int
+Mapper::rewrite_flowid(RewriterInput *, const IPFlowID &, IPFlowID &,
+                         Packet *, int)
+{
+    return RewriterBase::rw_drop;
+}
+
+
+ RewriterBase
+
+
+RewriterBase::RewriterBase()
+        : _map(0), _heap(new RewriterHeap), _gc_timer(gc_timer_hook, this)
+{
+    _timeouts[0] = default_timeout;
+    _timeouts[1] = default_guarantee;
+    _gc_interval_sec = default_gc_interval;
+}
+
+RewriterBase::~RewriterBase()
+{
+    if (_heap)
+        _heap->unuse();
+}
+
+
+int
+RewriterBase::parse_input_spec(const String &line, RewriterInput &is,
+                                 int input_number, ErrorHandler *errh)
+{
+    PrefixErrorHandler cerrh(errh, "input spec " + String(input_number) + ": ");
+    String word, rest;
+    if (!cp_word(line, &word, &rest))
+        return cerrh.error("empty argument");
+    cp_eat_space(rest);
+
+    is.kind = RewriterInput::i_drop;
+    is.owner = this;
+    is.owner_input = input_number;
+    is.reply_element = this;
+
+    if (word == "pass" || word == "passthrough" || word == "nochange") {
+        int32_t outnum = 0;
+        if (rest && !IntArg().parse(rest, outnum))
+            return cerrh.error("syntax error, expected %<nochange [OUTPUT]%>");
+        else if ((unsigned) outnum >= (unsigned) noutputs())
+            return cerrh.error("output port out of range");
+        is.kind = RewriterInput::i_nochange;
+        is.foutput = outnum;
+
+    } else if (word == "keep") {
+        Vector<String> words;
+        cp_spacevec(rest, words);
+        if (!IPRewriterPattern::parse_ports(words, &is, this, &cerrh))
+            return -1;
+        if ((unsigned) is.foutput >= (unsigned) noutputs()
+            || (unsigned) is.routput >= (unsigned) is.reply_element->noutputs())
+            return cerrh.error("output port out of range");
+        is.kind = RewriterInput::i_keep;
+
+    } else if (word == "drop" || word == "discard") {
+        if (rest)
+            return cerrh.error("syntax error, expected %<%s%>", word.c_str());
+
+    } else if (word == "pattern" || word == "xpattern") {
+        if (!IPRewriterPattern::parse_with_ports(rest, &is, this, &cerrh))
+            return -1;
+        if ((unsigned) is.foutput >= (unsigned) noutputs()
+            || (unsigned) is.routput >= (unsigned) is.reply_element->noutputs())
+            return cerrh.error("output port out of range");
+        is.u.pattern->use();
+        is.kind = RewriterInput::i_pattern;
+
+    } else if (Element *e = cp_element(word, this, 0)) {
+        Mapper *mapper = (Mapper *)e->cast("Mapper");
+        if (rest)
+            return cerrh.error("syntax error, expected element name");
+        else if (!mapper)
+            return cerrh.error("element is not an Mapper");
+        else {
+            is.kind = RewriterInput::i_mapper;
+            is.u.mapper = mapper;
+        }
+
+    } else
+        return cerrh.error("unknown specification");
+
+    return 0;
+}
+
+int
+RewriterBase::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    String capacity_word;
+
+    if (Args(this, errh).bind(conf)
+                .read("CAPACITY", AnyArg(), capacity_word)
+                .read("MAPPING_CAPACITY", AnyArg(), capacity_word)
+                .read("TIMEOUT", SecondsArg(), _timeouts[0])
+                .read("GUARANTEE", SecondsArg(), _timeouts[1])
+                .read("REAP_INTERVAL", SecondsArg(), _gc_interval_sec)
+                .read("REAP_TIME", Args::deprecated, SecondsArg(), _gc_interval_sec)
+                .consume() < 0)
+        return -1;
+
+    if (capacity_word) {
+        Element *e;
+        RewriterBase *rwb;
+        if (IntArg().parse(capacity_word, _heap->_capacity))
+            /* OK */;
+        else if ((e = cp_element(capacity_word, this))
+                 && (rwb = (RewriterBase *) e->cast("RewriterBase"))) {
+            rwb->_heap->use();
+            _heap->unuse();
+            _heap = rwb->_heap;
+        } else
+            return errh->error("bad MAPPING_CAPACITY");
+    }
+
+    if (conf.size() != ninputs())
+        return errh->error("need %d arguments, one per input port", ninputs());
+
+    _timeouts[0] *= CLICK_HZ;	// _timeouts is measured in jiffies
+    _timeouts[1] *= CLICK_HZ;
+
+    for (int i = 0; i < conf.size(); ++i) {
+        RewriterInput is;
+        if (parse_input_spec(conf[i], is, i, errh) >= 0)
+            _input_specs.push_back(is);
+    }
+
+    return _input_specs.size() == ninputs() ? 0 : -1;
+}
+
+int
+RewriterBase::initialize(ErrorHandler *errh)
+{
+    for (int i = 0; i < _input_specs.size(); ++i) {
+        PrefixErrorHandler cerrh(errh, "input spec " + String(i) + ": ");
+        if (_input_specs[i].reply_element->_heap != _heap)
+            cerrh.error("reply element %<%s%> must share this MAPPING_CAPACITY", i, _input_specs[i].reply_element->name().c_str());
+        if (_input_specs[i].kind == RewriterInput::i_mapper)
+            _input_specs[i].u.mapper->notify_rewriter(this, &_input_specs[i], &cerrh);
+    }
+    _gc_timer.initialize(this);
+    if (_gc_interval_sec)
+        _gc_timer.schedule_after_sec(_gc_interval_sec);
+    return errh->nerrors() ? -1 : 0;
+}
+
+void
+RewriterBase::cleanup(CleanupStage)
+{
+    shrink_heap(true);
+    for (int i = 0; i < _input_specs.size(); ++i)
+        if (_input_specs[i].kind == RewriterInput::i_pattern)
+            _input_specs[i].u.pattern->unuse();
+    _input_specs.clear();
+}
+
+IPRewriterEntry *
+RewriterBase::get_entry(int ip_p, const IPFlowID &flowid, int input)
+{
+    IPRewriterEntry *m = _map.get(flowid);
+    if (m && ip_p && m->flow()->ip_p() && m->flow()->ip_p() != ip_p)
+        return 0;
+    if (!m && (unsigned) input < (unsigned) _input_specs.size()) {
+        RewriterInput &is = _input_specs[input];
+        IPFlowID rewritten_flowid = IPFlowID::uninitialized_t();
+        if (is.rewrite_flowid(flowid, rewritten_flowid, 0) == rw_addmap)
+            m = add_flow(ip_p, flowid, rewritten_flowid, input);
+    }
+    return m;
+}
+
+IPRewriterEntry *
+RewriterBase::store_flow(RewriterFlow *flow, int input,
+                           Map &map, Map *reply_map_ptr)
+{
+    RewriterBase *reply_element = _input_specs[input].reply_element;
+    if ((unsigned) flow->entry(false).output() >= (unsigned) noutputs()
+        || (unsigned) flow->entry(true).output() >= (unsigned) reply_element->noutputs()) {
+        flow->owner()->owner->destroy_flow(flow);
+        return 0;
+    }
+
+    IPRewriterEntry *old = map.set(&flow->entry(false));
+    assert(!old);
+
+    if (!reply_map_ptr)
+        reply_map_ptr = &reply_element->_map;
+    old = reply_map_ptr->set(&flow->entry(true));
+    if (unlikely(old)) {		// Assume every map has the same heap.
+        if (likely(old->flow() != flow))
+            old->flow()->destroy(_heap);
+    }
+
+    Vector<RewriterFlow *> &myheap = _heap->_heaps[flow->guaranteed()];
+    myheap.push_back(flow);
+    push_heap(myheap.begin(), myheap.end(),
+              RewriterFlow::heap_less(), RewriterFlow::heap_place());
+    ++_input_specs[input].count;
+
+    if (unlikely(_heap->size() > _heap->capacity())) {
+        // This may destroy the newly added mapping, if it has the lowest
+        // expiration time.  How can we tell?  If (1) flows are added to the
+        // heap one at a time, so the heap was formerly no bigger than the
+        // capacity, and (2) 'flow' expires in the future, then we will only
+        // destroy 'flow' if it's the top of the heap.
+        click_jiffies_t now_j = click_jiffies();
+        assert(click_jiffies_less(now_j, flow->expiry())
+               && _heap->size() == _heap->capacity() + 1);
+        if (shrink_heap_for_new_flow(flow, now_j)) {
+            ++_input_specs[input].failures;
+            return 0;
+        }
+    }
+
+    if (map.unbalanced())
+        map.rehash(map.bucket_count() + 1);
+    if (reply_map_ptr != &map && reply_map_ptr->unbalanced())
+        reply_map_ptr->rehash(reply_map_ptr->bucket_count() + 1);
+    return &flow->entry(false);
+}
+
+void
+RewriterBase::shift_heap_best_effort(click_jiffies_t now_j)
+{
+    // Shift flows with expired guarantees to the best-effort heap.
+    Vector<RewriterFlow *> &guaranteed_heap = _heap->_heaps[1];
+    while (guaranteed_heap.size() && guaranteed_heap[0]->expired(now_j)) {
+        RewriterFlow *mf = guaranteed_heap[0];
+        click_jiffies_t new_expiry = mf->owner()->owner->best_effort_expiry(mf);
+        mf->change_expiry(_heap, false, new_expiry);
+    }
+}
+
+bool
+RewriterBase::shrink_heap_for_new_flow(RewriterFlow *flow,
+                                         click_jiffies_t now_j)
+{
+    shift_heap_best_effort(now_j);
+    // At this point, all flows in the guarantee heap expire in the future.
+    // So remove the next-to-expire best-effort flow, unless there are none.
+    // In that case we always remove the current flow to honor previous
+    // guarantees (= admission control).
+    RewriterFlow *deadf;
+    if (_heap->_heaps[0].empty()) {
+        assert(flow->guaranteed());
+        deadf = flow;
+    } else
+        deadf = _heap->_heaps[0][0];
+    deadf->destroy(_heap);
+    return deadf == flow;
+}
+
+void
+RewriterBase::shrink_heap(bool clear_all)
+{
+    click_jiffies_t now_j = click_jiffies();
+    shift_heap_best_effort(now_j);
+    Vector<RewriterFlow *> &best_effort_heap = _heap->_heaps[0];
+    while (best_effort_heap.size() && best_effort_heap[0]->expired(now_j))
+        best_effort_heap[0]->destroy(_heap);
+
+    int32_t capacity = clear_all ? 0 : _heap->_capacity;
+    while (_heap->size() > capacity) {
+        RewriterFlow *deadf = _heap->_heaps[_heap->_heaps[0].empty()][0];
+        deadf->destroy(_heap);
+    }
+}
+
+void
+RewriterBase::gc_timer_hook(Timer *t, void *user_data)
+{
+    RewriterBase *rw = static_cast<RewriterBase *>(user_data);
+    rw->shrink_heap(false);
+    if (rw->_gc_interval_sec)
+        t->reschedule_after_sec(rw->_gc_interval_sec);
+}
+
+String
+RewriterBase::read_handler(Element *e, void *user_data)
+{
+    RewriterBase *rw = static_cast<RewriterBase *>(e);
+    intptr_t what = reinterpret_cast<intptr_t>(user_data);
+    StringAccum sa;
+
+    switch (what) {
+        case h_nmappings: {
+            uint32_t count = 0;
+            for (int i = 0; i < rw->_input_specs.size(); ++i)
+                count += rw->_input_specs[i].count;
+            sa << count;
+            break;
+        }
+        case h_mapping_failures: {
+            uint32_t count = 0;
+            for (int i = 0; i < rw->_input_specs.size(); ++i)
+                count += rw->_input_specs[i].failures;
+            sa << count;
+            break;
+        }
+        case h_size:
+            sa << rw->_heap->size();
+            break;
+        case h_capacity:
+            sa << rw->_heap->_capacity;
+            break;
+        default:
+            for (int i = 0; i < rw->_input_specs.size(); ++i) {
+                if (what != h_patterns && what != i)
+                    continue;
+                switch (rw->_input_specs[i].kind) {
+                    case RewriterInput::i_drop:
+                        sa << "<drop>";
+                        break;
+                    case RewriterInput::i_nochange:
+                        sa << "<nochange>";
+                        break;
+                    case RewriterInput::i_keep:
+                        sa << "<keep>";
+                        break;
+                    case RewriterInput::i_pattern:
+                        sa << rw->_input_specs[i].u.pattern->unparse();
+                        break;
+                    case RewriterInput::i_mapper:
+                        sa << "<mapper>";
+                        break;
+                }
+                if (rw->_input_specs[i].count)
+                    sa << " [" << rw->_input_specs[i].count << ']';
+                sa << '\n';
+            }
+            break;
+    }
+    return sa.take_string();
+}
+
+int
+RewriterBase::write_handler(const String &str, Element *e, void *user_data, ErrorHandler *errh)
+{
+    RewriterBase *rw = static_cast<RewriterBase *>(e);
+    intptr_t what = reinterpret_cast<intptr_t>(user_data);
+    if (what == h_capacity) {
+        if (Args(e, errh).push_back_words(str)
+                                  .read_mp("CAPACITY", rw->_heap->_capacity)
+                                  .complete() < 0)
+        return -1;
+        rw->shrink_heap(false);
+        return 0;
+    } else if (what == h_clear) {
+        rw->shrink_heap(true);
+        return 0;
+    } else
+        return -1;
+}
+
+int
+RewriterBase::pattern_write_handler(const String &str, Element *e, void *user_data, ErrorHandler *errh)
+{
+    RewriterBase *rw = static_cast<RewriterBase *>(e);
+    intptr_t what = reinterpret_cast<intptr_t>(user_data);
+    RewriterInput is;
+    int r = rw->parse_input_spec(str, is, what, errh);
+    if (r >= 0) {
+        RewriterInput *spec = &rw->_input_specs[what];
+
+        // remove all existing flows created by this input
+        for (int which_heap = 0; which_heap < 2; ++which_heap) {
+            Vector<RewriterFlow *> &myheap = rw->_heap->_heaps[which_heap];
+            for (int i = myheap.size() - 1; i >= 0; --i)
+                if (myheap[i]->owner() == spec) {
+                    myheap[i]->destroy(rw->_heap);
+                    if (i < myheap.size())
+                        ++i;
+                }
+        }
+
+        // change pattern
+        if (spec->kind == RewriterInput::i_pattern)
+            spec->u.pattern->unuse();
+        *spec = is;
+    }
+    return 0;
+}
+
+void
+RewriterBase::add_rewriter_handlers(bool writable_patterns)
+{
+    add_read_handler("table_size", read_handler, h_nmappings);
+    add_read_handler("nmappings", read_handler, h_nmappings, Handler::h_deprecated);
+    add_read_handler("mapping_failures", read_handler, h_mapping_failures);
+    add_read_handler("patterns", read_handler, h_patterns);
+    add_read_handler("size", read_handler, h_size);
+    add_read_handler("capacity", read_handler, h_capacity);
+    add_write_handler("capacity", write_handler, h_capacity);
+    add_write_handler("clear", write_handler, h_clear);
+    for (int i = 0; i < ninputs(); ++i) {
+        String name = "pattern" + String(i);
+        add_read_handler(name, read_handler, i);
+        if (writable_patterns)
+            add_write_handler(name, pattern_write_handler, i);
+    }
+}
+
+int
+RewriterBase::llrpc(unsigned command, void *data)
+{
+    if (command == CLICK_LLRPC_IPREWRITER_MAP_TCP) {
+        // Data	: unsigned saddr, daddr; unsigned short sport, dport
+        // Incoming : the flow ID
+        // Outgoing : If there is a mapping for that flow ID, then stores the
+        //	      mapping into 'data' and returns zero. Otherwise, returns
+        //	      -EAGAIN.
+
+        IPFlowID *val = reinterpret_cast<IPFlowID *>(data);
+        IPRewriterEntry *m = get_entry(IP_PROTO_TCP, *val, -1);
+        if (!m)
+            return -EAGAIN;
+        *val = m->rewritten_flowid();
+        return 0;
+
+    } else if (command == CLICK_LLRPC_IPREWRITER_MAP_UDP) {
+        // Data	: unsigned saddr, daddr; unsigned short sport, dport
+        // Incoming : the flow ID
+        // Outgoing : If there is a mapping for that flow ID, then stores the
+        //	      mapping into 'data' and returns zero. Otherwise, returns
+        //	      -EAGAIN.
+
+        IPFlowID *val = reinterpret_cast<IPFlowID *>(data);
+        IPRewriterEntry *m = get_entry(IP_PROTO_UDP, *val, -1);
+        if (!m)
+            return -EAGAIN;
+        *val = m->rewritten_flowid();
+        return 0;
+
+    } else
+        return Element::llrpc(command, data);
+}
+
+ELEMENT_REQUIRES(IPRewriterMapping IPRewriterPattern)
+ELEMENT_PROVIDES(RewriterBase)
+CLICK_ENDDECLS
---
+++ rewriterbase.hh
@@ -0,0 +1,254 @@
+// -*- mode: c++; c-basic-offset: 4 -*-
+#ifndef CLICK_IPREWRITERBASE_HH
+#define CLICK_IPREWRITERBASE_HH
+#include <click/timer.hh>
+#include "../../elements/ip/iprwmapping.hh"
+#include <click/bitvector.hh>
+#include <click/ipflowid.hh>
+#include <click/ip
+#include <click/hashcontainer.hh>
+
+CLICK_DECLS
+class Mapper;
+class RewriterPattern;
+class RewriterBase;
+
+class RewriterInput { public:
+    enum {
+        i_drop, i_nochange, i_keep, i_pattern, i_mapper
+    };
+    RewriterBase *owner;
+    int owner_input;
+    int kind;
+    int foutput;
+    RewriterBase *reply_element;
+    int routput;
+    uint32_t count;
+    uint32_t failures;
+    union {
+        RewriterPattern *pattern;
+        Mapper *mapper;
+    } u;
+
+    RewriterInput()
+            : kind(i_drop), foutput(-1), routput(-1), count(0), failures(0) {
+        u.pattern = 0;
+    }
+
+    enum {
+        mapid_default = 0, mapid_iprewriter_udp = 1
+    };
+
+    inline int rewrite_flowid(const IPFlowID &flowid,
+                              IPFlowID &rewritten_flowid,
+                              Packet *p, int mapid = mapid_default);
+};
+
+class RewriterHeap { public:
+
+    RewriterHeap()
+            : _capacity(0x7FFFFFFF), _use_count(1) {
+    }
+    ~RewriterHeap() {
+        assert(size() == 0);
+    }
+
+    void use() {
+        ++_use_count;
+    }
+    void unuse() {
+        assert(_use_count > 0);
+        if (--_use_count == 0)
+            delete this;
+    }
+
+    Vector<RewriterFlow *>::size_type size() const {
+        return _heaps[0].size() + _heaps[1].size();
+    }
+    int32_t capacity() const {
+        return _capacity;
+    }
+
+private:
+
+    enum {
+        h_best_effort = 0, h_guarantee = 1
+    };
+    Vector<RewriterFlow *> _heaps[2];
+    int32_t _capacity;
+    uint32_t _use_count;
+
+    friend class RewriterBase;
+    friend class RewriterFlow;
+
+};
+
+class RewriterBase : public Element { public:
+
+    typedef HashContainer<IPRewriterEntry> Map;
+    enum {
+        rw_drop = -1, rw_addmap = -2
+    };
+
+    RewriterBase() CLICK_COLD;
+    ~RewriterBase() CLICK_COLD;
+
+    enum ConfigurePhase {
+        CONFIGURE_PHASE_PATTERNS = CONFIGURE_PHASE_INFO,
+        CONFIGURE_PHASE_REWRITER = CONFIGURE_PHASE_DEFAULT,
+        CONFIGURE_PHASE_MAPPER = CONFIGURE_PHASE_REWRITER - 1,
+        CONFIGURE_PHASE_USER = CONFIGURE_PHASE_REWRITER + 1
+    };
+
+    const char *port_count() const	{ return "1-/1-"; }
+    const char *processing() const	{ return PUSH; }
+
+    int configure_phase() const		{ return CONFIGURE_PHASE_REWRITER; }
+    int configure(Vector<String> &conf, ErrorHandler *errh) CLICK_COLD;
+    int initialize(ErrorHandler *errh) CLICK_COLD;
+    void add_rewriter_handlers(bool writable_patterns);
+    void cleanup(CleanupStage) CLICK_COLD;
+
+    const RewriterHeap *flow_heap() const {
+        return _heap;
+    }
+    RewriterBase *reply_element(int input) const {
+        return _input_specs[input].reply_element;
+    }
+    virtual HashContainer<IPRewriterEntry> *get_map(int mapid) {
+        return likely(mapid == RewriterInput::mapid_default) ? &_map : 0;
+    }
+
+    enum {
+        get_entry_check = -1, get_entry_reply = -2
+    };
+    virtual IPRewriterEntry *get_entry(int ip_p, const IPFlowID &flowid,
+                                       int input);
+    virtual IPRewriterEntry *add_flow(int ip_p, const IPFlowID &flowid,
+                                      const IPFlowID &rewritten_flowid,
+                                      int input) = 0;
+    virtual void destroy_flow(RewriterFlow *flow) = 0;
+    virtual click_jiffies_t best_effort_expiry(const RewriterFlow *flow) {
+        return flow->expiry() + _timeouts[0] - _timeouts[1];
+    }
+
+    int llrpc(unsigned command, void *data);
+
+protected:
+
+    Map _map;
+
+    Vector<RewriterInput> _input_specs;
+
+    RewriterHeap *_heap;
+    uint32_t _timeouts[2];
+    uint32_t _gc_interval_sec;
+    Timer _gc_timer;
+
+    enum {
+        default_timeout = 300,	   // 5 minutes
+        default_guarantee = 5,	   // 5 seconds
+        default_gc_interval = 60 * 15 // 15 minutes
+    };
+
+    static uint32_t relevant_timeout(const uint32_t timeouts[2]) {
+        return timeouts[1] ? timeouts[1] : timeouts[0];
+    }
+
+    IPRewriterEntry *store_flow(RewriterFlow *flow, int input,
+                                Map &map, Map *reply_map_ptr = 0);
+    inline void unmap_flow(RewriterFlow *flow,
+                           Map &map, Map *reply_map_ptr = 0);
+
+    static void gc_timer_hook(Timer *t, void *user_data);
+
+    int parse_input_spec(const String &str, RewriterInput &is,
+                         int input_number, ErrorHandler *errh);
+
+    enum {			// < 0 because individual patterns are >= 0
+        h_nmappings = -1, h_mapping_failures = -2, h_patterns = -3,
+        h_size = -4, h_capacity = -5, h_clear = -6
+    };
+    static String read_handler(Element *e, void *user_data) CLICK_COLD;
+    static int write_handler(const String &str, Element *e, void *user_data, ErrorHandler *errh) CLICK_COLD;
+    static int pattern_write_handler(const String &str, Element *e, void *user_data, ErrorHandler *errh) CLICK_COLD;
+
+    friend int RewriterInput::rewrite_flowid(const IPFlowID &flowid,
+                                               IPFlowID &rewritten_flowid, Packet *p, int mapid);
+
+private:
+
+    void shift_heap_best_effort(click_jiffies_t now_j);
+    bool shrink_heap_for_new_flow(RewriterFlow *flow, click_jiffies_t now_j);
+    void shrink_heap(bool clear_all);
+
+    friend class RewriterFlow;
+
+};
+
+
+class Mapper { public:
+
+    Mapper()				{ }
+    virtual ~Mapper()			{ }
+
+    virtual void notify_rewriter(RewriterBase *user, RewriterInput *input,
+                                 ErrorHandler *errh);
+    virtual int rewrite_flowid(RewriterInput *input,
+                               const IPFlowID &flowid,
+                               IPFlowID &rewritten_flowid,
+                               Packet *p, int mapid);
+
+};
+
+
+inline int
+RewriterInput::rewrite_flowid(const IPFlowID &flowid,
+                                IPFlowID &rewritten_flowid,
+                                Packet *p, int mapid)
+{
+    int i;
+    switch (kind) {
+        case i_nochange:
+            return foutput;
+        case i_keep:
+            rewritten_flowid = flowid;
+            return RewriterBase::rw_addmap;
+        case i_pattern: {
+            HashContainer<IPRewriterEntry> *reply_map;
+            if (likely(mapid == mapid_default))
+                reply_map = &reply_element->_map;
+            else
+                reply_map = reply_element->get_map(mapid);
+            i = u.pattern->rewrite_flowid(flowid, rewritten_flowid, *reply_map);
+            goto check_for_failure;
+        }
+        case i_mapper:
+            i = u.mapper->rewrite_flowid(this, flowid, rewritten_flowid, p, mapid);
+            goto check_for_failure;
+        check_for_failure:
+            if (i == RewriterBase::rw_drop)
+                ++failures;
+            return i;
+        default:
+            return RewriterBase::rw_drop;
+    }
+}
+
+inline void
+RewriterBase::unmap_flow(RewriterFlow *flow, Map &map,
+                           Map *reply_map_ptr)
+{
+    //click_chatter("kill %s", hashkey().s().c_str());
+    if (!reply_map_ptr)
+        reply_map_ptr = &flow->owner()->reply_element->_map;
+    Map::iterator it = map.find(flow->entry(0).hashkey());
+    if (it.get() == &flow->entry(0))
+        map.erase(it);
+    it = reply_map_ptr->find(flow->entry(1).hashkey());
+    if (it.get() == &flow->entry(1))
+        reply_map_ptr->erase(it);
+}
+
+CLICK_ENDDECLS
+#endif

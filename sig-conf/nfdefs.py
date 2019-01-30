0204969b5d3415f2e9bb7580313dea46c9c43cfc
miladghaznavi <miladghaznavi@gmail.com>
2019-01-29 23:53:09 -0500

    no per packet latency

--- sig-conf/nfdefs.py
+++ sig-conf/nfdefs.py
@@ -101,7 +101,7 @@ NAT_MB_PARAMS = ['$ip_min', '$ip_max']
 # NAT_MB_PARAMS_FORMAT = 'pattern {IP_MIN}-{IP_MAX} - {PORT} {PORT}'
 # NAT_MB_PARAMS_FORMAT = 'pattern {IP_MIN}-{IP_MAX} - 0 0'
 
-SNAPSHOT_MB = 'Snapshot(PERIOD 200, DELAY 6, PER_PACKET 0)'
+SNAPSHOT_MB = 'Snapshot(PERIOD 200, DELAY 6)'
 SNAPSHOT_MB_PARAMS = []
 
 NF_OUTPUT_NAME_FORMAT_STR = 'nf-{}.click'

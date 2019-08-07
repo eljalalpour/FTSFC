3f27ef4ae0c649091ca9eeba55000d5364bf297c
miladghaznavi <miladghaznavi@gmail.com>
2019-08-07 17:19:48 -0400

    FT Transaction element

---
+++ ft_tran.cc
@@ -0,0 +1,109 @@
+#include <click/config.h>
+#include <click/router.hh>
+#include <click/args.hh>
+#include "ft_tran.hh"
+
+CLICK_DECLS
+
+FTTran::FTTran () {
+    _runs = 0;
+    _aborts = 0;
+    _packets = 0;
+}
+
+FTTran::~FTTran() { }
+
+void FTTran::_init_shared_state() {
+    Router *r = this->router();
+    _shared_state = (FTSharedState *)(r->find("shared_state"));
+    _shared_locks = (SharedLocks *)(r->find("share_locks"));
+}
+
+void FTTran::_init_times(int conflict_prob) {
+    _lock_time = TOTAL_TIME * conflict_prob * 10;
+    _processing_time = TOTAL_TIME * (100 - conflict_prob) * 10;
+}
+
+int FTTran::configure(Vector<String> &conf, ErrorHandler *errh) {
+    // set index param
+    int conflict_prob; // between 1 to 99
+    if (Args(conf, this, errh)
+                .read("INDEX", _index)
+                .read("CONFLICT_PROB", conflict_prob)
+                .complete() < 0)
+        return -1;
+
+    DEBUG("FTTran index is %d!\n", _index);
+    DEBUG("FTTran conflict probability is %d!\n", conflict_prob);
+
+    _init_shared_state();
+    _init_times(conflict_prob);
+
+    return 0;
+}
+
+String FTTran::read_param(Element *e, void *thunk)
+{
+    FTTran *td = (ToDevice *)e;
+
+    String re = String(td->_runs);
+    re.append(",");
+    re.append(String(td->_aborts));
+    re.append(",");
+    re.append(String(td->_packets));
+
+    return re;
+}
+
+Packet *FTTran::simple_action(Packet *p) {
+    DEBUG("--------------------");
+    DEBUG("Begin FTTran with index %d:", _index);
+
+    _packets += 1;
+
+    int first = 0;
+    int second = 1;
+
+    if (_index % 2 == 1) {
+        first = 1;
+        second = 0;
+    }//if
+
+    BEGIN:
+    /// Track the number of transactions
+    _runs += 1;
+
+    /// Acquire lock for first
+    _shared_locks->lock(first);
+
+    /// Lock time: Wait for some time
+    Util::nsleep(_lock_time);
+
+    /// Acquire lock for second
+    if (_shared_locks->try_lock(second)) {
+        /// if successful, wait for some time
+        Util::nsleep(_processing_time);
+    }//if
+    else {
+        /// Abort transaction
+        /// Track the number of aborts
+        _aborts += 1;
+        _shared_locks->unlock(first);
+
+        /// Restart transaction
+        goto BEGIN;
+    }//else
+
+    /// Release locks
+    _shared_locks->unlock(second);
+    _shared_locks->unlock(first);
+
+
+    DEBUG("End FTTran %d:", _index);
+    DEBUG("--------------------");
+
+    return p;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(FTTran)
---
+++ ft_tran.hh
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "defs.hh"
+#include <click/config.h>
+#include <click/element.hh>
+#include "ft_shared_state.hh"
+#include "shared_locks.hh"
+
+CLICK_DECLS
+
+#define TOTAL_TIME 200 // micro-second
+
+
+class FTTran : public Element {
+private:
+    FTSharedState* _shared_state;
+    SharedLocks *_shared_locks;
+    int _index; // Must be 0 or 1
+    inline void _init_shared_state();
+    inline void _init_times(int conflict_prob);
+
+    long long _aborts;
+    long long _runs;
+    long long _packets;
+
+    int _lock_time;
+    int _processing_time;
+
+public:
+    FTTran ();
+
+    ~FTTran();
+
+    const char *class_name() const { return "FTTran"; }
+
+    const char *port_count() const { return PORTS_1_1; }
+
+    const char *processing() const { return AGNOSTIC; }
+
+    int configure(Vector<String> &conf, ErrorHandler *errh);
+
+    Packet *simple_action(Packet *p);
+
+    void add_handlers() CLICK_COLD;
+    static String read_param(Element *e, void *thunk) CLICK_COLD;
+};
+
+CLICK_ENDDECLS
